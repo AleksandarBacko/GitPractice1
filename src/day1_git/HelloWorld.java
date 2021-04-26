18604967d14cec8ca7b4b49424d1cc82d221541a
AleksandarBacko <backo.backovic94@gmail.com>
2021-04-26 13:54:26 -0700

    more on git

--- src/day1_git/HelloWorld.java
+++ src/day1_git/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
     public static void main(String[] args) {
         System.out.println("Hello World");
         System.out.println("adding github now");
+        System.out.println("yey stuff are on git" );
     }
 }
